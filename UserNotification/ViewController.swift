c5bd5fbbc5f57e4196ba2194ff34669d91acfe93
carlos-ios-developer <dr.carlos.iosdeveloper@gmail.com>
2018-01-18 00:40:07 +0200

    upload to git

--- UserNotification/ViewController.swift
+++ UserNotification/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+            
+            if error != nil {
+                print("Authorization Unsuccessfull")
+            }else {
+                print("Authorization Successfull")
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func Notify(_ sender: UIButton) {
+        timedNotifications(inSeconds: 10) { (success) in
+            if success {
+                print("Successfully Notified")
+            }
+        }
     }
-
-
+    
+    func timedNotifications(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title = "Breaking News"
+        content.subtitle = "user notification"
+        content.body = "Success"
+        
+        let request = UNNotificationRequest(identifier: "customNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if error != nil {
+                completion(false)
+            }else {
+                completion(true)
+            }
+        }
+    }
 }
+
+
+
 
